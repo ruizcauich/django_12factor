7abee7a6041d02505724ef448164c4966c0dc7cf
Augusto Neftalí Ruiz Cauich <augusto.ruizcauich@gmail.com>
2021-10-11 16:16:54 -0500

    Prepare the settings to store sensitive data in the environment.
    Based on 'Configuring django settings best practices' at
    https://djangostars.com/blog/configuring-django-settings-best-practices/

--- django_12factor/settings/django.py
+++ django_12factor/settings/django.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+env = environ.Env(
+    # set casting, default value
+    DJANGO_12FACTOR_DEBUG=(bool, False)
+)
+environ.Env.read_env()
+#
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#sp-ut*!4vp!-^5%ma9%dt&r#314##+g_synmpne0f!az=g^_5'
+SECRET_KEY = env('DJANGO_12FACTOR_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DJANGO_12FACTOR_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,10 +82,10 @@ WSGI_APPLICATION = 'django_12factor.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db_url(
+        'DJANGO_12FACTOR_DATABASE_URL',
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3'
+    )
 }
 
 
